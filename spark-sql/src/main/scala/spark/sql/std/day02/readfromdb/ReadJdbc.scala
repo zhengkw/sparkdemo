1d859f93ac9bd8228e2ea980fc13313ba3737468
zhengkw <swimer@vip.qq.com>
2020-05-14 15:52:07 +0800

    common

--- spark-sql/src/main/scala/spark/sql/std/day02/readfromdb/ReadJdbc.scala
+++ spark-sql/src/main/scala/spark/sql/std/day02/readfromdb/ReadJdbc.scala
@@ -14,17 +14,33 @@ import org.apache.spark.sql.SparkSession
  */
 object ReadJdbc {
   def main(args: Array[String]): Unit = {
-    val url = "jdbc:mysql://hadoop102:3306/rdd"
-    val props: Properties = new Properties()
-    props.setProperty("user","root")
-    props.setProperty("password","sa")
+    //方法1
+    /*  val url = "jdbc:mysql://hadoop102:3306/rdd"
+      val props: Properties = new Properties()
+      props.setProperty("user","root")
+      props.setProperty("password","sa")
 
+      val spark = SparkSession.builder()
+        .master("local[2]")
+        .appName("ReadJdbc ")
+        .getOrCreate()
+      import spark.implicits._
+
+      spark.read.jdbc(url,"user",props).show()
+      */
+    //方法2
     val spark = SparkSession.builder()
       .master("local[2]")
       .appName("ReadJdbc ")
       .getOrCreate()
     import spark.implicits._
-
-    spark.read.jdbc(url,"user",props).show()
+    val url = "jdbc:mysql://hadoop102:3306/rdd"
+    val dfr = spark.read.format("jdbc")
+      .option("user", "root")
+      .option("password", "sa")
+      .option("dbtable", "user")
+      .option("url", url)
+    val df = dfr.load()
+    df.show()
   }
 }
