9595ca5e9973cc3ce959c0de5a073e9c5bf38ccf
zhengkw <swimer@vip.qq.com>
2020-05-24 17:44:41 +0800

    rename

--- spark-sql/src/main/scala/spark/sql/std/day01/ScalaColl2DF_3.scala
+++ spark-sql/src/main/scala/spark/sql/std/day01/ScalaColl2DF_3.scala
@@ -3,18 +3,18 @@ package spark.sql.std.day01
 import org.apache.spark.sql.SparkSession
 
 /**
- * @ClassName:RDD2DF_3
+ * @ClassName:ScalaColl
  * @author: zhengkw
  * @description: 集合转换成DF
  * @date: 20/05/22下午 5:03
  * @version:1.0
  * @since: jdk 1.8 scala 2.11.8
  */
-object RDD2DF_3 {
+object ScalaColl2DF_3 {
   def main(args: Array[String]): Unit = {
     //创建一个builder，从builder或者取session
     val spark = SparkSession.builder()
-      .appName("DF2RDD_3")
+      .appName("ScalaColl")
       .master("local[2]")
       .getOrCreate()
     import spark.implicits._
